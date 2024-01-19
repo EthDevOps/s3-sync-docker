1fd10749050eeaff5d8a82d254641333311c18aa
Markus Keil <markus.keil@ethereum.org>
2024-01-19 18:11:55 +0100

    remove info call as they need more perms

--- mirror-s3.sh
+++ mirror-s3.sh
@@ -6,11 +6,5 @@ echo "Adding S3 source..."
 echo "Adding S3 destination..."
 /usr/bin/mc alias set s3_destination ${DESTINATION_ENDPOINT} ${DESTINATION_ACCESS_KEY} ${DESTINATION_SECRET_KEY}
 
-echo "Verifying source..."
-/usr/bin/mc admin info s3_source
-
-echo "Verifying source..."
-/usr/bin/mc admin info s3_destination
-
 echo "Start mirror..."
 /usr/bin/mc mirror --overwrite ${MINIO_EXTRA_OPTS} s3_source/${SOURCE_BUCKET} s3_destination/${DESTINATION_BUCKET} && curl ${HEALTHCHECK_URL}
