68703e07f4e8909c4647dc44b79c9d9a0b653005
Markus Keil <markus.keil@ethereum.org>
2024-01-24 08:12:18 +0100

    add debug mode

--- mirror-s3.sh
+++ mirror-s3.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+DEBUG=""
+
+if [[ "$MINIO_DEBUG" == "1" ]]; then
+    echo "== DEBUG ENABLED =="
+    DEBUG="--debug"
+fi
+
 
 echo "Adding S3 source..."
 /usr/bin/mc alias set s3_source ${SOURCE_ENDPOINT} ${SOURCE_ACCESS_KEY} ${SOURCE_SECRET_KEY}
@@ -7,4 +14,4 @@ echo "Adding S3 destination..."
 /usr/bin/mc alias set s3_destination ${DESTINATION_ENDPOINT} ${DESTINATION_ACCESS_KEY} ${DESTINATION_SECRET_KEY}
 
 echo "Start mirror..."
-/usr/bin/mc mirror --overwrite ${MINIO_EXTRA_OPTS} s3_source/${SOURCE_BUCKET} s3_destination/${DESTINATION_BUCKET} && curl ${HEALTHCHECK_URL}
+/usr/bin/mc ${DEBUG} mirror --overwrite ${MINIO_EXTRA_OPTS} s3_source/${SOURCE_BUCKET} s3_destination/${DESTINATION_BUCKET} && curl ${HEALTHCHECK_URL}
