0e28c01f34cb9ba88b7ec8d582c92a57888b5b37
Markus Keil <markus.keil@ethereum.org>
2024-01-24 11:03:46 +0100

    verwrite configurable

--- mirror-s3.sh
+++ mirror-s3.sh
@@ -6,6 +6,10 @@ if [[ "$MINIO_DEBUG" == "1" ]]; then
     DEBUG="--debug"
 fi
 
+if [[ "$MINIO_OVERWRITE" == "1" ]]; then
+    echo "== OVERWRITE ENABLED =="
+    OVERWRITE="--overwrite"
+fi
 
 echo "Adding S3 source..."
 /usr/bin/mc alias set s3_source ${SOURCE_ENDPOINT} ${SOURCE_ACCESS_KEY} ${SOURCE_SECRET_KEY}
@@ -14,4 +18,4 @@ echo "Adding S3 destination..."
 /usr/bin/mc alias set s3_destination ${DESTINATION_ENDPOINT} ${DESTINATION_ACCESS_KEY} ${DESTINATION_SECRET_KEY}
 
 echo "Start mirror..."
-/usr/bin/mc ${DEBUG} mirror --overwrite ${MINIO_EXTRA_OPTS} s3_source/${SOURCE_BUCKET} s3_destination/${DESTINATION_BUCKET} && curl ${HEALTHCHECK_URL}
+/usr/bin/mc ${DEBUG} mirror ${OVERWRITE} ${MINIO_EXTRA_OPTS} s3_source/${SOURCE_BUCKET} s3_destination/${DESTINATION_BUCKET} && curl ${HEALTHCHECK_URL}
